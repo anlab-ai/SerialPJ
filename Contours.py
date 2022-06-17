97f500f586c5cfa438d1fad1cb2bfeb7b32e5f47
dungtd <dung.td@anlab.asia>
2022-06-17 19:48:06 +0700

    serial recognize

---
+++ Contours.py
@@ -0,0 +1,391 @@
+from queue import Empty
+import cv2
+from cv2 import rectangle 
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+def sortBBox(listboxmax,listboxmin):
+    for i in range(len(listboxmax)):
+        for j in range(0,len(listboxmax)-i-1):
+            if listboxmax[j][0] > listboxmax[j+1][0]:
+                temp = listboxmax[j]
+                listboxmax[j] = listboxmax[j+1]
+                listboxmax[j+1] = temp
+               
+                temp1 = listboxmin[j]
+                listboxmin[j] = listboxmin[j+1]
+                listboxmin[j+1] = temp1
+    return listboxmax, listboxmin
+
+def padding(bin):
+    #bottom
+    for i in range(bin.shape[1]):
+        # for j in range(num):
+        bin[bin.shape[0]-2][i]=0
+        bin[bin.shape[0]-1][i]=0
+    #top
+    for i in range(bin.shape[1]):
+        bin[0][i]=0
+        bin[1][i]=0
+    #left
+    for i in range(bin.shape[0]):
+        bin[i][bin.shape[1]-2]=0
+        bin[i][bin.shape[1]-1]=0
+    #right
+    for i in range(bin.shape[0]):
+        bin[i][0]=0
+        bin[i][1]=0
+    return bin
+def coverGreenColorToWhiteColor(image):
+    height,width = image.shape[0],image.shape[1]
+    for loop1 in range(height):
+        for loop2 in range(width):
+            r,g,b = image[loop1,loop2]
+            if g > 1+r and g>1+b:
+                image[loop1,loop2] = 255,255,255
+    return image
+def getBoundingBox(contours):
+    listboxmin = []
+    listboxmax = []
+    for id in range(0,len(contours)):
+        listx=[]
+        listy=[]
+        for coor in contours[id]:
+            listx.append(coor[0][0])
+            listy.append(coor[0][1])
+        minx = np.min(listx)
+        miny = np.min(listy)
+        maxx = np.max(listx)
+        maxy = np.max(listy)
+        listboxmin.append((minx,miny))
+        listboxmax.append((maxx,maxy))
+    return listboxmax, listboxmin
+def drawBBox1(img,listboxmax,listboxmin):
+    for i in range(len(listboxmax)):
+        cv2.rectangle(img,listboxmin[i],listboxmax[i],(255,0,0),1)
+        plt.imshow(img)
+        plt.show()
+    return img
+def drawBBox(img,coor):
+    for i in range(len(coor)):
+        cv2.rectangle(img,coor[i][0],coor[i][1],(255,0,0),1)
+    return img
+def coverColorToWhiteColor(image):
+    height,width = image.shape[0],image.shape[1]
+    for loop1 in range(height):
+        for loop2 in range(width):
+            r,g,b = image[loop1,loop2]
+            if g -5 > r and g >= b:
+                image[loop1,loop2] = 255,255,255
+            elif g -10 > r and g + 1 <= b:
+                image[loop1,loop2] = 255,255,255
+            elif r >= g + 10 and r >= 10 + b:
+                image[loop1,loop2] = 255,255,255
+    return image
+def coverColorToWhiteColorMX(image):
+    height,width = image.shape[0],image.shape[1]
+    for loop1 in range(height):
+        for loop2 in range(width):
+            r,g,b = image[loop1,loop2]
+            if g > r +10 and g > b + 10:
+                image[loop1,loop2] = 255,255,255
+    return image    
+def delLine(image):
+    maxValue = 255*image.shape[1]
+    height,width = image.shape[0],image.shape[1]
+    sumRow = np.sum(image,axis=1)  
+    for loop1 in range(len(sumRow)):
+        if int(sumRow[loop1]) > maxValue*0.7:
+            for i in range(width):
+               image[loop1][i] = 0
+               if loop1 > 1:
+                   image[loop1-1][i] = 0
+                   image[loop1-2][i] = 0
+               if loop1 < height-2:
+                   image[loop1+1][i] = 0
+                   image[loop1+2][i] = 0
+    return image
+def getAvg(listboxmax,listboxmin):
+    area = []
+    areaHeight = []
+    areaWidth = []
+    for i in range(len(listboxmax)):
+        w = listboxmax[i][0] - listboxmin[i][0]
+        h = listboxmax[i][1] - listboxmin[i][1]
+        areaHeight.append(float(h))
+        areaWidth.append(float(w))
+        area.append(w*h)
+
+    avgAreaHeight= np.average(areaHeight)
+    avgAreaWidth= np.average(areaWidth)
+    avgArea = np.average(area)
+    return avgAreaWidth, avgAreaHeight, avgArea
+def avgDistanceChar(listboxmax,listboxmin):
+    distance = []
+    for i in range(len(listboxmax)-1):
+        distance.append(listboxmax[i+1][0] - listboxmin[i][0])
+    return np.average(distance)
+
+"""-----------------------API NEW-------------------------------"""
+def removeBadContours(image,listboxmax1,listboxmin1):
+    bettetContours = []
+    multiChar = []
+    listChar = []
+    listboxmax1,listboxmin1 = sortBBox(listboxmax1,listboxmin1)
+    avgAreaWidth, avgAreaHeight, avgArea = getAvg(listboxmax1,listboxmin1)
+    listboxmax2 = []
+    listboxmin2 = []
+    listboxmax = []
+    listboxmin = []
+    for i in range(len(listboxmax1)):
+        w = listboxmax1[i][0] - listboxmin1[i][0]
+        h = listboxmax1[i][1] - listboxmin1[i][1]
+        # if h * w >  0.03*avgArea  and w > 0.1*avgAreaWidth and h > 0.1* avgAreaHeight: 
+        listboxmax2.append((listboxmax1[i][0],listboxmax1[i][1]))
+        listboxmin2.append((listboxmin1[i][0],listboxmin1[i][1]))
+    avgAreaWidth, avgAreaHeight, avgArea = getAvg(listboxmax2,listboxmin2)
+    avgDistance = avgDistanceChar(listboxmax2,listboxmin2)
+    for i in range(len(listboxmax2)-1):
+        if listboxmin2[i+1][0] - listboxmax2[i][0] < avgDistance: 
+            listboxmax.append((listboxmax2[i][0],listboxmax2[i][1]))
+            listboxmin.append((listboxmin2[i][0],listboxmin2[i][1]))
+            if i + 1 == len(listboxmax2)-1:
+                listboxmax.append((listboxmax2[i + 1][0],listboxmax2[i + 1][1]))
+                listboxmin.append((listboxmin2[i + 1][0],listboxmin2[i + 1][1]))
+    avgAreaWidth, avgAreaHeight, avgArea = getAvg(listboxmax,listboxmin)
+    for i in range(len(listboxmax)):
+        w = listboxmax[i][0] - listboxmin[i][0]
+        h = listboxmax[i][1] - listboxmin[i][1]
+        # for file LK
+        # Normal number
+        if h > 0.65*avgAreaHeight and h * w >  0.5*avgArea: 
+            bettetContours.append([listboxmin[i],listboxmax[i]])
+        #Number 1
+        elif h > 0.6*avgAreaHeight and h > 1.5*w and w*h > 0.2*avgArea: 
+            bettetContours.append([listboxmin[i],listboxmax[i]])
+        #Number 0 small
+        elif abs(w-h)<5 and w*h > 0.2*avgArea: 
+            bettetContours.append([listboxmin[i],listboxmax[i]])
+        #for file MX
+        # if h > avgAreaHeight/1.5 and h * w > 1.2 * avgArea: 
+        #     bettetContours.append([listboxmin[i],listboxmax[i]])
+        # elif w < h*2.2 and h > avgAreaHeight/2.5 and h * w > 0.5*avgArea:
+        #     bettetContours.append([listboxmin[i],listboxmax[i]])
+    id = 0
+    for i in range(len(bettetContours)): 
+        w = bettetContours[i][1][0] - bettetContours[i][0][0] 
+        h = bettetContours[i][1][1] - bettetContours[i][0][1] 
+        if w >1.25*h and h * w > avgArea or w >1.8*h :
+            multiChar = [bettetContours[i][0],bettetContours[i][1]]
+            imgcp = image
+            imgcp = imgcp[multiChar[0][1]:multiChar[1][1],multiChar[0][0]:multiChar[1][0]]
+            w = multiChar[1][0] - multiChar[0][0] 
+            h = multiChar[1][1] - multiChar[0][1] 
+            if w > 0.5*avgAreaWidth and w < 2.5*avgAreaWidth :
+                imgcp2 = image[multiChar[0][1]:multiChar[1][1],multiChar[0][0]+int(w/2):multiChar[1][0]]
+                imgcp2 = padding(imgcp2)
+                contourss,hierachy=cv2.findContours(imgcp2,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+                bboxmax,bboxmin = getBoundingBox(contourss)
+                bbox = []
+                for k in range(len(bboxmax)):
+                    bbox.append([bboxmin[k],bboxmax[k]])
+                bigArea = 0
+                for l in bbox:
+                    w1 = l[1][0] - l[0][0]
+                    h1 = l[1][1] - l[0][1]
+                    if w1*h1 >bigArea:
+                        bigArea = w1*h1
+                if 2*bigArea > (w/2)*h:
+                    listChar.append([(multiChar[0][0]-2,multiChar[0][1]-2),(multiChar[1][0]-int(w/2)+2,multiChar[1][1]+2)])
+                    listChar.append([(multiChar[0][0]+int(w/2)-2,multiChar[0][1]-2),(multiChar[1][0]+2,multiChar[1][1]+2)])
+                else:
+                    listChar.append([(multiChar[0][0]-2,multiChar[0][1]-2),(multiChar[1][0]+2,multiChar[1][1]+2)])
+            elif w < 4*avgAreaWidth and w >= 2.5*avgAreaWidth:
+                listChar.append([(multiChar[0][0]-2,multiChar[0][1]-2),(multiChar[1][0]-int(w/3)*2+2,multiChar[1][1]+2)])
+                listChar.append([(multiChar[0][0]+int(w/3)-2,multiChar[0][1]-2),(multiChar[1][0]-int(w/3)+2,multiChar[1][1]+2)])
+                listChar.append([(multiChar[1][0]-int(w/3)-2,multiChar[0][1]-2),(multiChar[1][0]+2,multiChar[1][1]+2)])
+            
+        else:
+            listChar.append([(bettetContours[i][0][0],bettetContours[i][0][1]),(bettetContours[i][1][0],bettetContours[i][1][1])])
+    return listChar
+def splitCharFromSerialNo(image):
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    SerialNo = image
+    SerialGray = coverColorToWhiteColor(SerialNo)
+    SerialGray = cv2.cvtColor(SerialGray, cv2.COLOR_BGR2GRAY)
+    # Inverse 
+    m, dev = cv2.meanStdDev(SerialGray)
+    ret, thresh = cv2.threshold(SerialGray, m[0][0] - 0.5*dev[0][0], 255, cv2.THRESH_BINARY_INV)
+    thresh = delLine(thresh)
+    # Padding
+    thresh = padding(thresh)
+    
+    # Finding countours
+    contours,hierachy=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+    # plt.imshow(image)
+    # plt.show()
+    listboxmax,listboxmin = getBoundingBox(contours)
+    listChar = removeBadContours(thresh,listboxmax,listboxmin)
+    return image, listChar
+def splitCharFromForm(image,box=[1600,1380,2034,1460]):
+    sizeImg=[2114,2990]
+    image = cv2.resize(image,sizeImg)
+    SerialNo = image[box[1]:box[3],box[0]:box[2]]
+    # print(image.shape)
+    image, listChar = splitCharFromSerialNo(SerialNo)
+    return image, listChar
+# image = cv2.imread('input/MFG No.032200723 LK-11VC-02.jpg')
+# image,listChar = splitCharFromForm(image)
+# print(listChar)
+# image = drawBBox(image,listChar)
+# cv2.imwrite("drawing.jpg" ,image )
+# plt.imshow(image)
+# plt.show()
+
+"""--------------------------------------------------------------"""
+# def removeBadContours(filename,oriimg,image,listboxmax1,listboxmin1):
+#     bettetContours = []
+#     multiChar = []
+#     listChar = []
+#     listboxmax1,listboxmin1 = sortBBox(listboxmax1,listboxmin1)
+#     avgAreaWidth, avgAreaHeight, avgArea = getAvg(listboxmax1,listboxmin1)
+#     listboxmax2 = []
+#     listboxmin2 = []
+#     listboxmax = []
+#     listboxmin = []
+#     for i in range(len(listboxmax1)):
+#         w = listboxmax1[i][0] - listboxmin1[i][0]
+#         h = listboxmax1[i][1] - listboxmin1[i][1]
+#         # if h * w >  0.03*avgArea  and w > 0.1*avgAreaWidth and h > 0.1* avgAreaHeight: 
+#         listboxmax2.append((listboxmax1[i][0],listboxmax1[i][1]))
+#         listboxmin2.append((listboxmin1[i][0],listboxmin1[i][1]))
+#     avgAreaWidth, avgAreaHeight, avgArea = getAvg(listboxmax2,listboxmin2)
+#     avgDistance = avgDistanceChar(listboxmax2,listboxmin2)
+#     for i in range(len(listboxmax2)-1):
+#         if listboxmin2[i+1][0] - listboxmax2[i][0] < avgDistance: 
+#             listboxmax.append((listboxmax2[i][0],listboxmax2[i][1]))
+#             listboxmin.append((listboxmin2[i][0],listboxmin2[i][1]))
+#             if i + 1 == len(listboxmax2)-1:
+#                 listboxmax.append((listboxmax2[i + 1][0],listboxmax2[i + 1][1]))
+#                 listboxmin.append((listboxmin2[i + 1][0],listboxmin2[i + 1][1]))
+#     avgAreaWidth, avgAreaHeight, avgArea = getAvg(listboxmax,listboxmin)
+#     for i in range(len(listboxmax)):
+#         w = listboxmax[i][0] - listboxmin[i][0]
+#         h = listboxmax[i][1] - listboxmin[i][1]
+#         # for file LK
+#         # Normal number
+#         if h > 0.65*avgAreaHeight and h * w >  0.5*avgArea: 
+#             bettetContours.append([listboxmin[i],listboxmax[i]])
+#         #Number 1
+#         elif h > 0.6*avgAreaHeight and h > 1.5*w and w*h > 0.2*avgArea: 
+#             bettetContours.append([listboxmin[i],listboxmax[i]])
+#         #Number 0 small
+#         elif abs(w-h)<5 and w*h > 0.2*avgArea: 
+#             bettetContours.append([listboxmin[i],listboxmax[i]])
+#         #for file MX
+#         # if h > avgAreaHeight/1.5 and h * w > 1.2 * avgArea: 
+#         #     bettetContours.append([listboxmin[i],listboxmax[i]])
+#         # elif w < h*2.2 and h > avgAreaHeight/2.5 and h * w > 0.5*avgArea:
+#         #     bettetContours.append([listboxmin[i],listboxmax[i]])
+#     id = 0
+#     for i in range(len(bettetContours)): 
+#         w = bettetContours[i][1][0] - bettetContours[i][0][0] 
+#         h = bettetContours[i][1][1] - bettetContours[i][0][1] 
+#         if w >1.25*h and h * w > avgArea or w >1.8*h :
+#             multiChar = [bettetContours[i][0],bettetContours[i][1]]
+#             imgcp = image
+#             imgcp = imgcp[multiChar[0][1]:multiChar[1][1],multiChar[0][0]:multiChar[1][0]]
+#             w = multiChar[1][0] - multiChar[0][0] 
+#             h = multiChar[1][1] - multiChar[0][1] 
+#             if w > 0.5*avgAreaWidth and w < 2.5*avgAreaWidth :
+#                 imgcp2 = image[multiChar[0][1]:multiChar[1][1],multiChar[0][0]+int(w/2):multiChar[1][0]]
+#                 imgcp2 = padding(imgcp2)
+#                 contourss,hierachy=cv2.findContours(imgcp2,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+#                 bboxmax,bboxmin = getBoundingBox(contourss)
+#                 bbox = []
+#                 for k in range(len(bboxmax)):
+#                     bbox.append([bboxmin[k],bboxmax[k]])
+#                 bigArea = 0
+#                 for l in bbox:
+#                     w1 = l[1][0] - l[0][0]
+#                     h1 = l[1][1] - l[0][1]
+#                     if w1*h1 >bigArea:
+#                         bigArea = w1*h1
+#                 if 2*bigArea > (w/2)*h:
+#                     img1 = oriimg[multiChar[0][1]-2:multiChar[1][1]+2,multiChar[0][0]-2:multiChar[1][0]-int(w/2)+2]
+#                     cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/ResultCrop/'+folderName+'/'+str(id)+'_'+filename,img1)
+#                     id+=1
+#                     img2 = oriimg[multiChar[0][1]-2:multiChar[1][1]+2,multiChar[0][0]+int(w/2)-2:multiChar[1][0]+2]
+#                     cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/ResultCrop/'+folderName+'/'+str(id)+'_'+filename,img2)
+#                     id+=1
+#                     listChar.append([(multiChar[0][0]-2,multiChar[0][1]-2),(multiChar[1][0]-int(w/2)+2,multiChar[1][1]+2)])
+#                     listChar.append([(multiChar[0][0]+int(w/2)-2,multiChar[0][1]-2),(multiChar[1][0]+2,multiChar[1][1]+2)])
+#                 else:
+#                     listChar.append([(multiChar[0][0]-2,multiChar[0][1]-2),(multiChar[1][0]+2,multiChar[1][1]+2)])
+#                     img = oriimg[multiChar[0][1]-2:multiChar[1][1]+2,multiChar[0][0]-2:multiChar[1][0]+2]
+#                     cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/ResultCrop/'+folderName+'/'+str(id)+'_'+filename,img)
+#                     id+=1
+#             elif w < 4*avgAreaWidth and w >= 2.5*avgAreaWidth:
+#                 listChar.append([(multiChar[0][0]-2,multiChar[0][1]-2),(multiChar[1][0]-int(w/3)*2+2,multiChar[1][1]+2)])
+#                 listChar.append([(multiChar[0][0]+int(w/3)-2,multiChar[0][1]-2),(multiChar[1][0]-int(w/3)+2,multiChar[1][1]+2)])
+#                 listChar.append([(multiChar[1][0]-int(w/3)-2,multiChar[0][1]-2),(multiChar[1][0]+2,multiChar[1][1]+2)])
+#                 img1 = oriimg[multiChar[0][1]-2:multiChar[1][1]+2,multiChar[0][0]-2:multiChar[1][0]-int(w/3)*2+2]
+#                 cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/ResultCrop/'+folderName+'/'+str(id)+'_'+filename,img1)
+#                 id+=1
+#                 img2 = oriimg[multiChar[0][1]-2:multiChar[1][1]+2,multiChar[0][0]+int(w/3)-2:multiChar[1][0]-int(w/3)+2]
+#                 cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/ResultCrop/'+folderName+'/'+str(id)+'_'+filename,img2)
+#                 id+=1
+#                 img3 = oriimg[multiChar[0][1]-2:multiChar[1][1]+2,multiChar[1][0]-int(w/3)-2:multiChar[1][0]+2]
+#                 cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/ResultCrop/'+folderName+'/'+str(id)+'_'+filename,img3)
+#                 id+=1
+            
+#         else:
+#             listChar.append([(bettetContours[i][0][0],bettetContours[i][0][1]),(bettetContours[i][1][0],bettetContours[i][1][1])])
+#             img = oriimg[bettetContours[i][0][1]:bettetContours[i][1][1],bettetContours[i][0][0]:bettetContours[i][1][0]]
+#             cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/ResultCrop/'+folderName+'/'+str(id)+'_'+filename,img)
+#             id+=1
+#     return bettetContours
+# def splitCharFromSerialNo(image,filename):
+#     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+#     SerialNo = image
+#     SerialGray = coverColorToWhiteColorMX(SerialNo)
+#     SerialGray = cv2.cvtColor(SerialGray, cv2.COLOR_BGR2GRAY)
+#     # Inverse 
+#     m, dev = cv2.meanStdDev(SerialGray)
+#     ret, thresh = cv2.threshold(SerialGray, m[0][0] - 0.5*dev[0][0], 255, cv2.THRESH_BINARY_INV)
+#     thresh = delLine(thresh)
+#     # Padding
+#     cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/flag/'+filename,thresh)
+#     thresh = padding(thresh)
+    
+#     # Finding countours
+#     contours,hierachy=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+#     # cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+#     # plt.imshow(image)
+#     # plt.show()
+#     listboxmax,listboxmin = getBoundingBox(contours)
+#     listChar = removeBadContours(filename,image,thresh,listboxmax,listboxmin)
+#     return listChar
+# folderName = 'MX'
+# for filename in os.listdir('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/TestImage/input-20220616T103931Z-001/input/'+folderName):
+#     image = cv2.imread('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/TestImage/input-20220616T103931Z-001/input/'+folderName+'/'+filename)
+#     #LK((1600,1380),(2034,1460)) size(2114,2990)
+#     #MX((1600,1785),(2000,1850)) size(2114,2990)
+#     #MXM((1850,1910),(2340,1985)) size(2480,3508)
+#     #SMX((1840,2000),(2290,2080)) size(2480,3508)
+#     sizeImg = (2114,2990)
+#     x1 = 1600
+#     y1 = 1785
+#     x2 = 2000
+#     y2 = 1850
+#     image = cv2.resize(image,sizeImg)
+#     SerialNo = image[y1:y2,x1:x2]
+#     listChar = splitCharFromSerialNo(SerialNo,filename)
+#     img = drawBBox(SerialNo,listChar)
+#     cv2.imwrite('/home/anlab/ANLAB/VisualCode/Contours/SplitHandWriting/OutPut/flag/'+filename,img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
---
+++ SerialDetection.py
@@ -0,0 +1,178 @@
+# set the matplotlib backend so figures can be saved in the background
+from posixpath import split
+import matplotlib
+matplotlib.use("Agg")
+import csv
+# import the necessary packages
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+from keras.models import model_from_json
+
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import os
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+import Contours 
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
+config = tf.ConfigProto(gpu_options=gpu_options)
+config.gpu_options.allow_growth = True
+
+sess = tf.Session(config=config)
+set_session(sess)
+import common
+
+
+def alphaBlend( img1, img2, mask):
+		if mask.ndim==3 and mask.shape[-1] == 3:
+			alpha = mask/255.0
+		else:
+			alpha = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)/255.0
+		blended = cv2.convertScaleAbs(img1*(1-alpha) + img2*alpha )
+		return blended
+
+def resize_image( image,max_w=128, max_h=128):
+	height,width= image.shape[:2]
+	scale_1 = float(max_w/ width)
+	scale_2 = float(max_h/ height)
+	scale = min(scale_1, scale_2)
+	width= int(width*scale)
+	height=int(height*scale)
+	image= cv2.resize(image,(width,height))
+	return image
+
+def get_background(image , mask):
+	h, w = image.shape[:2]
+	gray = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
+	m = cv2.mean(gray, mask)
+	_, th = cv2.threshold(gray,m[0],255,cv2.THRESH_BINARY_INV)
+	th = cv2.bitwise_and(mask, th)
+	m = cv2.mean(image, th)
+	output = np.zeros((h,w,3), image.dtype)
+	output[:, :] = (1*m[0], 1*m[1] ,1* m[2])
+	m = cv2.mean(output)
+	kernel = np.ones((3, 3), np.uint8)
+	mask = cv2.erode(mask, kernel) 
+	mask = cv2.GaussianBlur(mask, (5,5), 0)
+	blended = alphaBlend( output, image, mask)
+	# blended = cv2.GaussianBlur(blended, (3,3), 0)
+	# cv2.imwrite("blended.jpg" , th)
+   
+	return blended
+
+def create_mask(image, est=0.3):
+	h, w = image.shape[:2]
+	mask = 255*np.ones((h,w),np.uint8)
+	ex = 0
+	ey = 0
+	size = max(w, h)
+	if h > w:
+		ex = h-w
+	elif w> h:
+		ey = w- h
+	ex += int(est*size)
+	ey += int(est*size)
+	mask = cv2.copyMakeBorder(mask, ey//2, ey//2, ex//2, ex//2, cv2.BORDER_CONSTANT, value=(0, 0 , 0))
+	image = cv2.copyMakeBorder(image, ey//2, ey//2, ex//2, ex//2, cv2.BORDER_CONSTANT, value=(0, 0 , 0))
+	# mask = cv2.rectangle(mask, (0,0), (w, h), (0), 1)
+	return image, mask
+
+class SerialDetection():
+	def __init__(self):
+		self.num_classes = 36
+		# input image dimensions
+		self.img_rows, self.img_cols = 28, 28
+		json_file = open('models/model_0616_crop.json', 'r')
+		loaded_model_json = json_file.read()
+		json_file.close()
+		self.loaded_model = model_from_json(loaded_model_json)
+		# load weights into new model
+		self.loaded_model.load_weights("models/model_weights_0616_crop.h5")
+		print("Loaded model from disk")
+		self.loaded_model.summary()
+		self.img_debug = None
+		with open('model_name.txt','r') as f:
+			labels = [line.strip('\n') for line in f.readlines()]
+		self.labels = labels[0].split(",")
+	def predict(self, image, pose=-1):
+		image = cv2.bitwise_not(image)
+		# image = common.preprocess(image)
+		image = resize_image(image)
+		image, mask = create_mask(image)
+		
+		image = get_background(image, mask)
+		gray = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
+		gray = cv2.resize(gray, (self.img_rows, self.img_cols))
+		
+		gray = img_to_array(gray)
+		gray = np.array(gray, dtype="float") / 255.0
+		gray = np.expand_dims(gray, axis=0)
+		prediction = self.loaded_model.predict(gray)[0]
+		# print("prediction " , prediction, image.shape)
+		bestclass = ''
+		bestconf = -1
+		idx_cls = -1
+		for n in range(self.num_classes):
+			if pose > 1 and n > 9 :
+				break
+			if (prediction[n] > bestconf):
+				idx_cls = n
+				bestclass = self.labels[n]
+				bestconf = prediction[n]
+		return idx_cls, bestclass, bestconf
+	
+	def getSerialForm(self, image):
+		img_serial , listChar = Contours.splitCharFromForm(image)
+		# print("listChar" , len(listChar) , listChar)
+		serial_number = "" 
+		for i, box in enumerate(listChar):
+			xmin = box[0][0]
+			ymin = box[0][1]
+			xmax = box[1][0]
+			ymax = box[1][1]
+			im_char = img_serial[ymin:ymax, xmin:xmax]
+			idx_cls, bestclass, bestconf = model.predict(im_char , pose= i)
+			serial_number += bestclass
+			# cv2.rectangle(img_serial,(xmin, ymin),(xmax, ymax),(255,0,0),1)
+		
+		return img_serial , serial_number
+		
+if __name__ == '__main__':
+	model = SerialDetection()
+	imagePaths = sorted(list(paths.list_images("/media/anlab/ssd_samsung_256/dungtd/SplitHandWriting/TestImage/input")))
+	folder_save = "results"
+	if not os.path.exists(folder_save):
+		os.mkdir(folder_save)
+	for imagePath in imagePaths:
+		print("path " ,  imagePath)
+		basename = os.path.basename(imagePath)
+		image = cv2.imread(imagePath)
+		img_serial , serial_number = model.getSerialForm(image)
+		
+		path_out =os.path.join(folder_save , f'{serial_number}_{basename}')
+		cv2.imwrite(path_out, img_serial)
+		exit()
+ 
+
+ 
+	# imagePaths = sorted(list(paths.list_images("/media/anlab/ssd_samsung_256/dungtd/EMNIST/source/DataCrop/ResultCrop")))
+	# folder_save = "results"
+	# if not os.path.exists(folder_save):
+	# 	os.mkdir(folder_save)
+	# for imagePath in imagePaths:
+	# 	image = cv2.imread(imagePath)
+	# 	base_path = imagePath.split(os.path.sep)[-1]
+	# 	pose = int(base_path.split("_")[0])
+	# 	idx_cls, bestclass, bestconf = model.predict(image , pose= pose)
+	# 	img_out = model.img_debug
+	# 	path_out =os.path.join(folder_save , f'{bestclass}_{imagePath.split(os.path.sep)[-1]}') 
+	
+	# 	cv2.imwrite(path_out ,img_out )
+
