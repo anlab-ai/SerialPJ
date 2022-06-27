583932f6006ea73926c1816924766efcd05d2dfc
ThuyTran <thuy.tq@anlab.asia>
2022-06-27 14:24:36 +0700

    Update parameters in form InOut

--- Contours.py
+++ Contours.py
@@ -321,7 +321,7 @@ def getBBoxFromInOut(image,listboxmax,listboxmin):
     for i in range(len(listboxmax)):
         w = listboxmax1[i][0] - listboxmin1[i][0]
         h = listboxmax1[i][1] - listboxmin1[i][1]
-        if w*h > 0.01*image.shape[0]*image.shape[1]:
+        if w*h > 0.005*image.shape[0]*image.shape[1]:
             listBoxFilterSmall.append((listboxmin[i],listboxmax[i]))
             coorXmin.append(listboxmin1[i][0])
             coorXmax.append(listboxmax1[i][0])
