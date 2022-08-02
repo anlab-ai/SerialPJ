b181dff8861e091eebb22a0c8a970be22500efe6
longnm <long.nm@anlab.biz>
2022-08-02 17:24:52 +0700

    update config tess digits

--- tess_recognizer.py
+++ tess_recognizer.py
@@ -4,7 +4,7 @@ import pytesseract
 class TessRecognizer():
 	def __init__(self):
 		self.tess_config_eng = r'-l eng --psm 7 --oem 1'
-		self.tess_config_digits = r'-l digits --psm 10 --oem 1'
+		self.tess_config_digits = r'-l digits --psm 10 --oem 3'
 		self.tess_config_japanese = r'-l japanese --psm 7 --oem 1 -c tessedit_char_blacklist=ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 		self.tess_config_hiragana = r'-l japanese --psm 7 --oem 1 -c tessedit_char_whitelist=あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑを'
 		self.tess_config_japanese_digits = r'-l japanese --psm 7 --oem 1 -c tessedit_char_whitelist=1234567890-'
